b0f229039d3640d21141eacf844a72731c46bbf2
KartikBhamidipati <115195771+KartikBhamidipati@users.noreply.github.com>
2025-02-12 10:23:19 +0530

    AboutSec component added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Team from "../components/About/Team";
 import Footer from "../components/Footer";
+import AboutSec from "../components/About/AboutSec";
 
 const About = () => {
   return (
     <div className="bg-gray-900 h-auto">
         <NavBar />
+
+      {/* team section goes here */}
+      <div className="mt-20">
+      <AboutSec/>
+      </div>
+        
 
       {/* team section goes here */}
         <div className="mx-11">
